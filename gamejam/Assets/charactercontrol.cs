a68181e0941da8a6bd19660b751f1ed95f4b0936
kazu215 <21511252kh@tama.ac.jp>
2017-02-08 05:04:43 +0900

    02/08 05:00

--- gamejam/Assets/charactercontrol.cs
+++ gamejam/Assets/charactercontrol.cs
@@ -2,37 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class charactercontrol : MonoBehaviour {
+public class charactercontrol : MonoBehaviour
+{
+    private Vector3 velocity;
+    public float speed = 10;
 
-    public static float speed = 1.0f;
-
-    // Use this for initialization
-    void Start () {
+    void Start()
+    {
         DontDestroyOnLoad(this);
     }
-	
-	// Update is called once per frame
-	void FixedUpdate() {
 
+    void FixedUpdate()
+    {
         float H = Input.GetAxis("Horizontal");
         float V = Input.GetAxis("Vertical");
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        Transform transform = GetComponent<Transform>();
+        velocity = new Vector3(H,0,V);
+        Physics.gravity = new Vector3(0, 9.81f, 0);
+        transform.localPosition += velocity * speed;
 
-        rigidbody.velocity = Quaternion.Euler(0, H, 0) * rigidbody.velocity;
-
-        Vector3 front = rigidbody.velocity;
-
-        if (front.sqrMagnitude ==0)
-            return;
-        front.Normalize();
-        rigidbody.AddForce(front * V * speed);
-
-        if (rigidbody.velocity.sqrMagnitude > 1.0)
-            rigidbody.velocity.Normalize();
-
-       if(H==0 && V==0)
-        {
-            rigidbody.velocity = rigidbody.velocity * 0.97f;
-        } 
     }
 }
