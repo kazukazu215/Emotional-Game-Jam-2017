2739a0b349b77d20da58bc4db70c70c9ae36ac1a
kazu215 <21511252kh@tama.ac.jp>
2017-02-08 21:40:04 +0900

    tuika

---
+++ gamejam/Assets/warp2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class warp2 : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("2");
+        if (other.tag == "Aground")
+        {
+            this.transform.position = new Vector3(240, 2, 240);
+        }
+        else if (other.tag == "Bground")
+        {
+            this.transform.position = new Vector3(111, 2, 401);
+        }
+        else if (other.tag == "Cground")
+        {
+            this.transform.position = new Vector3(381, 2, 85);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ gamejam/Assets/warps.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class warps : MonoBehaviour {
+/*
+    public Button Button1;
+    public Button Button2;
+    public Button Button3;
+
+
+    void Start()
+    {
+        Button btn1 = Button1.GetComponent<Button>();
+        Button btn2 = Button2.GetComponent<Button>();
+        Button btn3 = Button3.GetComponent<Button>();
+
+    }
+
+
+    // Use this for initialization
+    public void OnButtonClick()
+    {
+        if (1)
+        {
+            GetComponent<warp>();
+        }
+        else if (n==2)
+        {
+            GetComponent<warp2>();
+        }
+        else if (n == 3)
+        {
+            GetComponent<warp3>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    */
+}
